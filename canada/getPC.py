fa547110aa9cd24aaee6ee1f5aeb461bd5a6d68a
saltire sable <x@saltiresable.com>
2014-05-31 11:21:31 -0400

    Fix indentation, use psycopg2 for postgres

--- canada/getPC.py
+++ canada/getPC.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
 import sys
-import pg
+import psycopg2 as pg
 import httplib
 import time
 
 def validatePostalCode(postalCode):
     r = True
-    if len(postalCode) == 5: #zipcode? 
-	r = False
+    if len(postalCode) == 5: #zipcode?
+        r = False
     elif len(postalCode) <> 6:
-	r = False
+        r = False
     else:
-    	for i in range(6):
-	    if i % 2: #  == 1 si impair
-		if not postalCode[i].isdigit(): # doit etre un chiffre
-		    r = False
-	    else:
-		if not postalCode[i].isalpha(): # doit etre une lettre
-		    r = False
+        for i in range(6):
+            if i % 2: #  == 1 si impair
+                if not postalCode[i].isdigit(): # doit etre un chiffre
+                    r = False
+            else:
+                if not postalCode[i].isalpha(): # doit etre une lettre
+                    r = False
     return r
 
 if len(sys.argv) > 1:
@@ -27,27 +27,27 @@ if len(sys.argv) > 1:
     pc_rows = pgconn.query("SELECT postalcode FROM postalcodes WHERE the_geom IS NULL LIMIT %d " % limit)
     pcs = pc_rows.getresult()
     for pc in pcs:
-	q = pc[0]
-	if validatePostalCode(q):
-	    time.sleep(1)
-	    key = 'GOOGLE_MAPS_API_KEY'
-	    path = "/maps/geo?q=%(q)s&gl=ca&sensor=false&output=csv&key=%(key)s" % {'q' : q, 'key' : key}
-	    conn = httplib.HTTPConnection("maps.google.com", timeout=5)
-	    try:
-		conn.request("GET", path)
-	    except (Exception):
-		print "socket.gaierror: timeout"
-		continue
-	    r = conn.getresponse()
-	    if r.status == 200:
-		a = r.read().split(',')
-		acc = a[1]
-		lat = a[2]
-		lng = a[3]
-		print '%(status)s: %(latlng)s (%(q)s) ' % {'status' : r.reason, 'latlng' : lat + ',' + lng, 'q': q}
-		wkt = "POINT(" + lng + " " + lat + ")"
-		pgconn.query("UPDATE postalcodes SET the_geom = ST_GeomFromText('%(wkt)s',4326) WHERE postalcode = '%(pc)s' " % { 'wkt': wkt, 'pc': pc[0]})
-	    else:
-		print 'error: %s' % r.status, r.reason
-	else:
-	    print 'not postal code: %s' % q
+        q = pc[0]
+        if validatePostalCode(q):
+            time.sleep(1)
+            key = 'GOOGLE_MAPS_API_KEY'
+            path = "/maps/geo?q=%(q)s&gl=ca&sensor=false&output=csv&key=%(key)s" % {'q' : q, 'key' : key}
+            conn = httplib.HTTPConnection("maps.google.com", timeout=5)
+            try:
+                conn.request("GET", path)
+            except (Exception):
+                print "socket.gaierror: timeout"
+                continue
+            r = conn.getresponse()
+            if r.status == 200:
+                a = r.read().split(',')
+                acc = a[1]
+                lat = a[2]
+                lng = a[3]
+                print '%(status)s: %(latlng)s (%(q)s) ' % {'status' : r.reason, 'latlng' : lat + ',' + lng, 'q': q}
+                wkt = "POINT(" + lng + " " + lat + ")"
+                pgconn.query("UPDATE postalcodes SET the_geom = ST_GeomFromText('%(wkt)s',4326) WHERE postalcode = '%(pc)s' " % { 'wkt': wkt, 'pc': pc[0]})
+            else:
+                print 'error: %s' % r.status, r.reason
+        else:
+            print 'not postal code: %s' % q
